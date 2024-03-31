5f4c030584045070a927ebfe4ea18634960d95a0
Zlatko Jovanovski <zlatko.jovanovski@students.finki.ukim.mk>
2024-03-31 20:30:05 +0200

    Created the following models Ingredient, Menu, NotIncluded, Recipe, RecipeMenu, ShippingAddress and an empty Cart model because ShippingAddress uses it for foreign key.
    Signed-off-by: Zlatko Jovanovski <zlatko.jovanovski@students.finki.ukim.mk>

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import *
 
-
 admin.site.register(Buyer, )
 admin.site.register(Staff, )
 admin.site.register(NutrientsChart, )
+admin.site.register(Ingredient, )
+admin.site.register(NotIncluded, )
+admin.site.register(Menu, )
+admin.site.register(Recipe, )
+admin.site.register(RecipeMenu, )
+admin.site.register(ShippingAddress, )
--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -29,3 +31,67 @@ class NutrientsChart(models.Model):
     protein = models.IntegerField(null=True, blank=True)
     cholesterol = models.IntegerField(null=True, blank=True)
     sodium = models.IntegerField(null=True, blank=True)
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class NotIncluded(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=255)
+    img = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+    subheading = models.CharField(max_length=255)
+    description = models.TextField()
+    difficulty = models.CharField(max_length=255, null=True, blank=True)
+    allergens = models.CharField(max_length=255, null=True, blank=True)
+    tags = models.CharField(max_length=255, null=True, blank=True)
+    total_time = models.IntegerField(null=True, blank=True)
+    pic = models.ImageField(null=True, blank=True)
+    price = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class RecipeMenu(models.Model):
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f' {self.recipe}  {self.menu} '
+
+
+class Cart(models.Model):
+    pass
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Buyer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Cart, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    country = models.CharField(max_length=200, null=False)
+    zipcode = models.CharField(max_length=200, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+    shipping_time_start = models.TimeField(default=datetime.time(15, 00))
+    shipping_time_end = models.TimeField(default=datetime.time(19, 00))
+
+    def __str__(self):
+        return self.address
