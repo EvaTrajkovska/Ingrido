5d41181109b235b580ed4879fb00e0317e30b2e7
Katerina <smilevskakaterina5@gmail.com>
2024-04-17 15:05:36 +0200

    Create the menu view.

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from app.models import Menu, RecipeMenu
+
+
 # Create your views here.
+
+def menu(request):
+    menuId = request.session['menuId']
+    menu = Menu.objects.get(id=menuId)
+    qs = RecipeMenu.objects.filter(menu_id=menuId).all()
+    context = {"recipes": qs, "menu": menu}
+    return render(request, "Menu.html", context)
